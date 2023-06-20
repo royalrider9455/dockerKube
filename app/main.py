d4f42fa2020cc3b4e2eafd41be54b7477a306f16
Manoj <manoj.kumar9455@gmail.com>
2023-06-20 14:43:55 +0530

    Removed Redis

--- app/main.py
+++ app/main.py
@@ -1,12 +1,8 @@
 from flask import Flask, request, render_template
-import redis
 
 app = Flask(__name__)
 
 import os
-
-REDIS_HOST = os.environ['REDIS_HOST']
-REDIS_PORT = os.environ['REDIS_PORT']
 
 POSTGRES_HOST = os.environ['POSTGRES_HOST']
 POSTGRES_USER = os.environ['POSTGRES_USER']
@@ -17,7 +13,7 @@ POSTGRES_PASSWORD = os.environ['PGPASSWORD']
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
 # postgresql://username:password@host:port/database
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://hello_flask:hello_flask@db:5432/hello_flask_dev'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://docker_kube:docker_kube@db:5432/docker_kube_dev'
 
 from models import db, UserFavs
 
@@ -27,7 +23,6 @@ with app.app_context():
     db.create_all()
     db.session.commit()
 
-red = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 
 @app.route("/")
 def main():
@@ -39,42 +34,25 @@ def save():
     place = str(request.form['place']).lower()
     food = str(request.form['food']).lower()
 
-    # check if data of the username already exists in the redis
-    if red.hgetall(username).keys():
-        print("hget username:", red.hgetall(username))
-        # return a msg to the template, saying the user already exists(from redis)
-        return render_template('index.html', user_exists=1, msg='(From Redis)', username=username, place=red.hget(username,"place").decode('utf-8'), food=red.hget(username,"food").decode('utf-8'))
-
-    # if not in redis, then check in db
-    elif len(list(red.hgetall(username)))==0:
-        record =  UserFavs.query.filter_by(username=username).first()
-        print("Records fecthed from db:", record)
+    record =  UserFavs.query.filter_by(username=username).first()
         
-        if record:
-            red.hset(username, "place", place)
-            red.hset(username, "food", food)
-            # return a msg to the template, saying the user already exists(from database)
-            return render_template('index.html', user_exists=1, msg='(From DataBase)', username=username, place=record.place, food=record.food)
+    if record:
+        # return a msg to the template, saying the user already exists(from database)
+        return render_template('index.html', user_exists=1, msg='(From DataBase)', username=username, place=record.place, food=record.food)
 
-    # if data of the username doesnot exist anywhere, create a new record in DataBase and store in Redis also
+    # if data of the username doesnot exist anywhere, create a new record in DataBase
     # create a new record in DataBase
     new_record = UserFavs(username=username, place=place, food=food)
     db.session.add(new_record)
     db.session.commit()
 
-    # store in Redis also
-    red.hset(username, "place", place)
-    red.hset(username, "food", food)
-
     # cross-checking if the record insertion was successful into database
     record =  UserFavs.query.filter_by(username=username).first()
     print("Records fetched from db after insert:", record)
 
-    # cross-checking if the insertion was successful into redis
-    print("key-values from redis after insert:", red.hgetall(username))
-
     # return a success message upon saving
-    return render_template('index.html', saved=1, username=username, place=red.hget(username, "place").decode('utf-8'), food=red.hget(username, "food").decode('utf-8'))
+    
+    return render_template('index.html', saved=1, username=username, place=record.place, food=record.food)
 
 @app.route("/keys", methods=['GET'])
 def keys():
@@ -89,16 +67,11 @@ def keys():
 def get():
 	username = request.form['username']
 	print("Username:", username)
-	user_data = red.hgetall(username)
-	print("GET Redis:", user_data)
+        
+	existing_record = UserFavs.query.filter_by(username=username).first()
+    
 
-	if not user_data:
-		record = UserFavs.query.filter_by(username=username).first()
-		print("GET Record:", record)
-		if not record:
-			print("No data in redis or db")
-			return render_template('index.html', no_record=1, msg=f"Record not yet defined for {username}")
-		red.hset(username, "place", record.place)
-		red.hset(username, "food", record.food)
-		return render_template('index.html', get=1, msg="(From DataBase)",username=username, place=record.place, food=record.food)
-	return render_template('index.html',get=1, msg="(From Redis)", username=username, place=user_data[b'place'].decode('utf-8'), food=user_data[b'food'].decode('utf-8'))
+	if not existing_record:		
+		return render_template('index.html', no_record=1, msg=f"Record not yet defined for {username}")
+		
+	return render_template('index.html', get=1, msg="(From DataBase)",username=username, place=existing_record.place, food=existing_record.food)
